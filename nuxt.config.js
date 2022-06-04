9bf8fbc518a9734cc8035391245244d8b21973fc
lovingmemory <106819138+lovingmemory@users.noreply.github.com>
2022-06-04 15:40:06 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
-const name = 'Janet Doe'
-const dates = 'July 29th 1958 - Jan 4th 2021'
+const name = 'June Brown'
+const dates = 'Feb 16th 1927 - Apr 3rd 2022'
 
 export default {
   publicRuntimeConfig: {
